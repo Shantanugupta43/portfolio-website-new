62d84c2bab4e955d39310c86aaea3b7ddc018ff9
Shantanu Gupta <99300527+Shantanugupta43@users.noreply.github.com>
2023-04-17 22:06:13 +0100

    Initial commit

---
+++ fade.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', function () {
+  var links = document.getElementsByTagName("a");
+  var content = document.getElementsByClassName("intro")[0];
+
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      console.log("clicked");
+      
+      if (content) {
+        content.classList.add("fade-out");
+        console.log("start");
+
+        setTimeout(function() {
+          content.classList.remove("fade-out");
+          window.location.href = event.target.href;
+        }, 1500);
+
+        console.log("done");
+      }
+
+      // perform some action here
+    }, false);
+  }
+});
