71b9c7a43067ac78dedbc483abbb7a04abbbb741
Shantanu Gupta <99300527+Shantanugupta43@users.noreply.github.com>
2023-04-18 19:45:22 +0100

    Background and click audio added

--- fade.js
+++ fade.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', function () {
   var links = document.getElementsByTagName("a");
   var content = document.getElementsByClassName("intro")[0];
+  var audio = new Audio("buttonclick.mp3");
+  
 
   for (var i = 0; i < links.length; i++) {
     links[i].addEventListener("click", function(event) {
@@ -9,17 +11,18 @@ window.addEventListener('DOMContentLoaded', function () {
       
       if (content) {
         content.classList.add("fade-out");
+        audio.play();
         console.log("start");
 
         setTimeout(function() {
           content.classList.remove("fade-out");
+          audio.pause();
           window.location.href = event.target.href;
         }, 1000);
 
         console.log("done");
       }
 
-      // perform some action here
     }, false);
   }
 });
